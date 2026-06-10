34a5188048692f68c9bc7e999c40ab905d2a51d5
NJ Smith <134447517+njsmithfm@users.noreply.github.com>
2026-06-10 14:07:58 -0400

    feat: debug +page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,14 @@
   import Timeline from "$lib/components/Timeline.svelte";
   import Map from "$lib/components/Map.svelte";
 
-  let activeView = "timeline";
+  let activeView = $state("timeline");
 </script>
 
 <h1>In The Crowd</h1>
 
 <div class="tabs">
-  <button on:click={() => (activeView = "timeline")}>Timeline</button>
-  <button on:click={() => (activeView = "map")}>Map</button>
+  <button onclick={() => (activeView = "timeline")}>Timeline</button>
+  <button onclick={() => (activeView = "map")}>Map</button>
 </div>
 
 {#if activeView === "timeline"}

0e8c01539e7d83f5b3b8a5fcd5ae9112cb0300a0
NJ Smith <134447517+njsmithfm@users.noreply.github.com>
2026-06-10 13:43:05 -0400

    feat: add content to +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,19 @@
+<script>
+  import Timeline from "$lib/components/Timeline.svelte";
+  import Map from "$lib/components/Map.svelte";
+
+  let activeView = "timeline";
+</script>
+
 <h1>In The Crowd</h1>
-<p>
-  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-  documentation
-</p>
+
+<div class="tabs">
+  <button on:click={() => (activeView = "timeline")}>Timeline</button>
+  <button on:click={() => (activeView = "map")}>Map</button>
+</div>
+
+{#if activeView === "timeline"}
+  <Timeline />
+{:else if activeView === "map"}
+  <Map />
+{/if}

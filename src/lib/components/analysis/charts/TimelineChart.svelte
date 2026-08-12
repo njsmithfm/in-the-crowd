74e0b3b8c40ffbfbeec96fcbe00bde25041bde4b
NJ Smith <134447517+njsmithfm@users.noreply.github.com>
2026-08-12 16:18:08 -0400

    feat: begin charting beeswarm in Timeline

--- src/lib/components/analysis/charts/TimelineChart.svelte
+++ src/lib/components/analysis/charts/TimelineChart.svelte
@@ -1,6 +1,6 @@
 <script>
   import { onMount } from "svelte";
-  import * as d3 from "d3";
+  import d3 from "d3";
   import BaseChartPanel from "$lib/components/analysis/charts/BaseChartPanel.svelte";
 
   let { data = [] } = $props();
@@ -15,36 +15,26 @@
     const innerHeight = height - margin.top - margin.bottom;
 
     const x = d3
-      .scaleBand()
-      .domain(data.map((d) => d.label))
-      .range([0, innerWidth])
-      .padding(0.2);
+      .scaleTime()
+      .domain(d3.extent(data, (d) => d.date))
+      .range([0, innerWidth]);
 
+    const years = Array.from(new Set(data.map((d) => d.year))).sort();
     const y = d3
-      .scaleLinear()
-      .domain([0, d3.max(data, (d) => d.value) ?? 0])
-      .nice()
-      .range([innerHeight, 0]);
+      .scalePoint()
+      .domain(years)
+      .range([innerHeight, 0])
+      .padding(0.5);
 
-    const root = d3.select(svg);
-    root.selectAll("*").remove();
-
-    const g = root
-      .attr("viewBox", `0 0 ${width} ${height}`)
-      .append("g")
-      .attr("transform", `translate(${margin.left},${margin.top})`);
-
-    g.selectAll("rect")
-      .data(data)
-      .join("rect")
-      .attr("x", (d) => x(d.label))
-      .attr("y", (d) => y(d.value))
-      .attr("width", x.bandwidth())
-      .attr("height", (d) => innerHeight - y(d.value))
-      .attr("fill", "#111");
+    const simulation = d3
+      .forceSimulation(data)
+      .force("x", d3.forceX((d) => x(d.date)).strength(1))
+      .force("y", d3.forceY((d) => y(d.year)).strength(0.6))
+      .force("collide", d3.forceCollide(5))
+      .stop();
   });
 </script>
 
-<BaseChartPanel title="Timeline" subtitle="(Rocking through time)">
+<BaseChartPanel title="Timeline" subtitle="(I rock through time)">
   <svg bind:this={svg} class="chart-svg"></svg>
 </BaseChartPanel>
